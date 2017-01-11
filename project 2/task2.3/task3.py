2c4d8f940cb46fd7e44a255c4d2d24c41a6a2837
V1nni <matyash.phil@gmail.com>
2017-01-12 00:58:19 +0100

    edited

--- project 2/task2.3/task3.py
+++ project 2/task2.3/task3.py
@@ -2,7 +2,9 @@ import math
 import numpy as np
 from PIL import Image as im
 def main(sigma = 0.5, photo ='phil'):
-    alfa = [1.68, -0.68]
+    import time
+    startT=time.time()
+    alfa = [1.68, -0.6803]
     beta = [3.7350 , -0.2598 ]
     gamma = [1.7830 ,1.7230 ]
     omega = [0.6318, 1.9970]
@@ -39,8 +41,10 @@ def main(sigma = 0.5, photo ='phil'):
     image1 = np.append(image[:,:4],image1,axis=1)
     image2 = np.append(image1, image1[(image1.shape[0] - 4):, :], axis=0)
     image2 = np.append(image1[:4,:], image2 , axis=0)
-    yminus = image2.copy()
-    yplus = image2.copy()
+    yminus = image2.copy()#np.zeros(image2.shape)
+    #yminus.fill(255.0)
+    yplus = yminus.copy()
+
     y = np.zeros(image2.shape)
     y.fill(255.0)
 
@@ -49,22 +53,23 @@ def main(sigma = 0.5, photo ='phil'):
             yplus[k,n] = sum([aplus[m] * image2[k,n-m] for m in range(4)]) -sum([bplus[m] * yplus[k,n-m] for m in range(1,5)])
             yminus[k,n] =sum([aminus[m] * image2[k,n+m] for m in range(1,5)]) -\
                     sum([bminus[m] * yminus[k,n+m] for m in range(1,5)])
-            y[k,n] = (yminus[k,n] + yplus[k,n])/(sigma *math.sqrt(2*math.pi))
+    y = (yminus + yplus)/(sigma *math.sqrt(2*math.pi))
     y1 = y.copy()
     for n in np.arange(w) + 4:
         for k in np.arange(h)+4:
             yplus[k,n] = sum([aplus[m] * y[k-m,n] for m in range(4)]) -sum([bplus[m] * yplus[k-m,n] for m in range(1,5)])
+        for k in reversed(np.arange(h)+4):
             yminus[k,n] =sum([aminus[m] * y[k+m,n] for m in range(1,5)]) -\
                     sum([bminus[m] * yminus[k+m,n] for m in range(1,5)])
-            y1[k,n] = (yminus[k,n] + yplus[k,n])/(sigma *math.sqrt(2*math.pi))
+    y1 = (yminus + yplus)/(sigma *math.sqrt(2*math.pi))
     if False:
         for n in np.arange(w) + 4:
             for k in np.arange(h) + 4:
-                yplus[k, n] = sum([aplus[m] * y[k - m, n] for m in range(4)]) - sum(
+                yplus[k, n] = sum([aplus[m] * image2[k - m, n] for m in range(4)]) - sum(
                     [bplus[m] * yplus[k - m, n] for m in range(1, 5)])
-                yminus[k, n] = sum([aminus[m] * y[k + m, n] for m in range(1, 5)]) - \
+                yminus[k, n] = sum([aminus[m] * image2[k + m, n] for m in range(1, 5)]) - \
                                sum([bminus[m] * yminus[k + m, n] for m in range(1, 5)])
-                y1[k, n] = (yminus[k, n] + yplus[k, n]) / (sigma * math.sqrt(2 * math.pi))
+                y[k, n] = (yminus[k, n] + yplus[k, n]) / (sigma * math.sqrt(2 * math.pi))
 
         y1 = y.copy()
         for k in np.arange(h) + 4:
@@ -76,13 +81,13 @@ def main(sigma = 0.5, photo ='phil'):
                 y1[k, n] = (yminus[k, n] + yplus[k, n]) / (sigma * math.sqrt(2 * math.pi))
 
     import scipy.misc as msc
-
+    y1[np.where(y1>255)] = 255
     im1 = msc.toimage(
         y1
     )
     im1.show()
-    im1.save('task2.3/%s_sigma%s.jpg' % (photo,sigma))
-
+    im1.save('task2.3/%s_sigma%s1.jpg' % (photo,sigma))
+    print(str(time.time() - startT) + " seconds")
     if __name__ == '__main__':
         for photo in ['bauckhage', 'phil','clock']:
             for s in [0.2,0.8,1.2]:
