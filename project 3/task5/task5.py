3aed0c34a1855929dcd53dc22f28716a17477a72
V1nni <matyash.phil@gmail.com>
2017-02-02 01:26:38 +0100

    added staff

--- project 3/task5/task5.py
+++ project 3/task5/task5.py
@@ -1,7 +1,10 @@
 import scipy.misc as msc
 import scipy.ndimage as img
 import numpy as np
-
+from skimage import data, color
+from skimage.feature import canny
+from skimage.transform import hough_ellipse
+from skimage.draw import ellipse_perimeter
 def getCoordinatePairs(img):
     hight,width = img.shape
     X = np.array(np.meshgrid(np.arange(width), np.arange(hight))).T.reshape(-1, 2)
@@ -20,12 +23,12 @@ def inpoly(xy, points):
     return ind
 
 
-def main(isle,asterix):
-
-    corrpoints = [(0,0), (0,asterix.shape[1]-1),
-                  tuple(s - 1 for s in asterix.shape),(asterix.shape[0]-1, 0)]
-    points = [(56, 215),(10, 365),
-              (296,364 ),(258, 218)]
+def main(isle,asterix, points = None, corrpoints = None, method = None):
+    if points is None:
+        corrpoints = [(0,0), (0,asterix.shape[1]-1),
+                      tuple(s - 1 for s in asterix.shape),(asterix.shape[0]-1, 0)]
+        points = [(56, 215),(10, 365),
+                  (296,364 ),(258, 218)]
     u =np.array(corrpoints).astype(float)[:,0]
     v= np.array(corrpoints).astype(float)[:,1]
     x = np.array(points).astype(float)[:,0]
@@ -47,33 +50,39 @@ def main(isle,asterix):
         xnew = (a*u+b*v + c)/(g*u+h*v+1)
         ynew = (d*u + e*v+f)/(g*u + h*v + 1)
         return  np.vstack((xnew,ynew))
-
-#    X = getCoordinatePairs(asterix).T
-    #calculate corresponding coords on isle
-#    new = getCoords(X[0],X[1])
-
-#    new = np.round(new).astype(int)
-
-    #very naive approach, just to round the mapped coordinates and put the intensities on them, works though
-    #for i in range(len(new.T)):
-    #    isle[new[0,i], new[1,i]] = asterix[X[0,i],X[1,i]]
-    #im2 = msc.toimage(isle, cmin=0,cmax=255)#.save('asterix-eye.png')
-    #im2.show()
-
-
     def invertCoord(x,y):
         newx = (b * f - c * e + (e - f * h) * x + (c * h - b) * y) \
                / (a * e - b * d + (d * h - e * g) * x + (b * g - a * h) * y)
         newy = (c * d - a * f + (f * g - d) * x + (a - c * g) * y) \
                / (a * e - b * d + (d * h - e * g) * x + (b * g - a * h) * y)
         return  np.vstack((newx,newy))
-    #dummy way of getting corresponding coordinates in "isle" pic
-#   xyIn = np.vstack({tuple(row) for row in new.T})
 
-    #better way to get coordinates from area in "isle"
-    #points = [(56, 215), (10, 365), (296, 364), (258, 218)]
-    xy = getCoordinatePairs(isle)
-    xyIn = xy[inpoly(xy, points).astype(bool)]
+    #calculate corresponding coords on isle
+
+    if method == 'naive':
+        X = getCoordinatePairs(asterix)
+        #only for clock approach, hack though
+        center = (corrpoints[0] +corrpoints[2])/2
+        r = np.sqrt(np.sum((corrpoints[0] - center)**2))
+        X = X[np.sqrt(np.sum((X - center) ** 2, axis=1)) < r].T
+        new = getCoords(X[0],X[1])
+        new = np.round(new).astype(int)
+        xyIn = np.vstack({tuple(row) for row in new.T})
+        #very naive approach, just to round the mapped coordinates and put the intensities on them, works though
+#        for i in range(len(new.T)):
+ #           isle[new[0,i], new[1,i]] = asterix[X[0,i],X[1,i]]
+    #im2 = msc.toimage(isle, cmin=0,cmax=255)#.save('asterix-eye.png')
+    #im2.show()
+    else:
+        # better way to get coordinates from area in "isle"
+        # points = [(56, 215), (10, 365), (296, 364), (258, 218)]
+        xy = getCoordinatePairs(isle)
+        xyIn = xy[inpoly(xy, points).astype(bool)]
+
+
+        #dummy way of getting corresponding coordinates in "isle" pic
+    #   xyIn = np.vstack({tuple(row) for row in new.T})
+
 
     #find coordinates in source picture by invert transform
     mapX = invertCoord(xyIn[:,0], xyIn[:,1])
@@ -85,6 +94,37 @@ def main(isle,asterix):
     im2 = msc.toimage(isle, cmin=0, cmax=255)  # .save('asterix-eye.png')
     #im2.show()
     return im2
+def getElipsePoints(src = None):
+
+
+    # Load picture, convert to grayscale and detect edges
+    if src is None :
+        image_rgb = data.coffee()[0:220, 160:420]
+        src = color.rgb2gray(image_rgb)
+    edges = canny(src, sigma=2.0,
+                  low_threshold=0.55, high_threshold=0.8)
+
+    # Perform a Hough Transform
+    result = hough_ellipse(edges, accuracy=20, threshold=250,
+                           min_size=100, max_size=120)
+    result.sort(order='accumulator')
+
+    # Estimated parameters for the ellipse
+    best = list(result[-1])
+    yc, xc, a, b = [int(round(x)) for x in best[1:5]]
+    orientation = best[5]
+
+    # Draw the ellipse on the original image
+    cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
+    # Find the most distant coordinates, i.e. biggest and smallest horde
+    xy = np.vstack((cy, cx)).T
+    center = np.array([yc, xc])
+    dists = np.sqrt(np.sum((xy - center) ** 2, axis=1))
+    p1 = xy[np.argmin(dists)]
+    p2 = xy[np.argmax(dists)]
+    p3 = 2 * center - p1
+    p4 = 2 * center - p2
+    return (p1,p2,p3,p4)
 if __name__ == '__main__':
     isle = msc.imread('task5/isle.jpg', flatten=True).astype('float')
     #let asterix be abstract pic to be projected
@@ -100,3 +140,13 @@ if __name__ == '__main__':
 
     im2.show()
     im2.save("iterated.jpg")
+    points = getElipsePoints()
+    image_rgb = data.coffee()[0:220, 160:420]
+    cup = np.array(color.rgb2gray(image_rgb)) *255
+    pairs = getCoordinatePairs(clock)
+    corrpoints = np.array([[127, 8], [3, 127], [127,245],[252,127]])
+    for p in points:
+        cup[p[0], p[1]] = 255
+    im3 = main(cup, clock, points, corrpoints,method='naive')
+    im3.show()
+    msc.toimage(cup).save('task5/cup.jpg')
